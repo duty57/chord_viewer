5b67f11068238042267f4a5dbe5e7248633eddce
Yevhen <yevhen.fedyniak@student.wat.edu.pl>
2025-11-07 13:07:29 +0100

    add a Divider component

---
+++ frontend/src/components/Divider.vue
@@ -0,0 +1,16 @@
+﻿<script setup lang="ts">
+
+</script>
+
+<template>
+<hr class="solid">
+</template>
+
+<style scoped>
+.solid {
+  width: 80%;
+  border: 1px solid var(--border-color);
+  border-radius: 25%;
+  margin: 2.5vh 0;
+}
+</style>
